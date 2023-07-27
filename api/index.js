e030273645127927c573bf2b91379367245a1c36
DelHoyoLorenzo <delhoyo.lorenzo@gmail.com>
2023-07-26 22:38:39 -0300

    update deploy

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 const createTypes = require('./src/Seeders/createTypes.js')
 const port = process.env.PORT || 3001
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true   }).then(() => {
   server.listen(port, () => {
     createTypes() //cada vez que se inicie el sv se crea la tabla types
     console.log('Server listening at Port 3001'); // eslint-disable-line no-console
