3a466bd7938c49d0cf662f0e87fd6651636169b5
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2023-09-06 13:23:55 -0300

    update

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import { searchById, cleanDetail } from "../../redux/actions";
 import style from "./Detail.module.css";
 import Loading from "../../components/Loading/Loading";
-import {TiBackspace} from 'react-icons/ti'
+import { TiBackspace } from "react-icons/ti";
 function Detail() {
   const { id } = useParams();
   let navigate = useNavigate();
@@ -14,7 +14,8 @@ function Detail() {
 
   let pokemonFoundById = useSelector((state) => state.pokemonFoundById);
   let loading = useSelector((state) => state.loading);
-  const { name, image, types, hp, attack, defense, speed, height, weight } = pokemonFoundById;
+  const { name, image, types, hp, attack, defense, speed, height, weight } =
+    pokemonFoundById;
 
   useEffect(() => {
     dispatch(searchById(id));
@@ -27,41 +28,49 @@ function Detail() {
 
   if (loading) return <Loading />;
   return (
-    <div>
-      <div className={style.background}>
+    <div className={style.background}>
       <div>
-        <TiBackspace className={style.icon} onClick={navigateHome}>Back</TiBackspace>
+        <TiBackspace className={style.icon} onClick={navigateHome}/>
       </div>
-        <div className={style.firstContainer}>
+      <div className={style.firstContainer}>
+        <div className={style.nameDiv}>
           <h1 className={style.nameContainer}>{name}</h1>
-          <img className={style.imagen} src={image} />
-          <div className={style.typesContainer}>
-            {types?.map((type) => {
-              return <h2>{type.name}</h2>;
-            })}
-          </div>
         </div>
-        <div className={style.secondContainer}>
-          <div>
-
-            <h2 className={style.everyDetailStyle}>id: {id}</h2>
-          </div>
-          <div>
-
+        <img className={style.imagen} src={image} />
+        <div className={style.typesContainer}>
+          {types?.map((type) => {
+            return <h2>{type.name}</h2>;
+          })}
+        </div>
+      </div>
+      <div className={style.secondContainer}>
+        <div>
+          <h2 className={style.everyDetailStyle}>id: {id}</h2>
+        </div>
+        <div>
           <h2 className={style.everyDetailStyle}>hp:{hp}</h2>
-          </div>
-          <div>
-
-          <h2 className={style.everyDetailStyle}>attack:{attack}</h2>
-          </div>
-          <div>
-
-          <h2 className={style.everyDetailStyle}>defense: {defense}</h2>
-          </div>
-          {speed !== 0 && <div><h2 className={style.everyDetailStyle}>speed: {speed}</h2></div>}
-          {weight !== 0 && <div><h2 className={style.everyDetailStyle}>weight: {weight}</h2></div>}
-          {height !== 0 && <div><h2 className={style.everyDetailStyle}>height: {height}</h2></div>}
         </div>
+        <div>
+          <h2 className={style.everyDetailStyle}>attack:{attack}</h2>
+        </div>
+        <div>
+          <h2 className={style.everyDetailStyle}>defense: {defense}</h2>
+        </div>
+        {speed !== 0 && (
+          <div>
+            <h2 className={style.everyDetailStyle}>speed: {speed}</h2>
+          </div>
+        )}
+        {weight !== 0 && (
+          <div>
+            <h2 className={style.everyDetailStyle}>weight: {weight}</h2>
+          </div>
+        )}
+        {height !== 0 && (
+          <div>
+            <h2 className={style.everyDetailStyle}>height: {height}</h2>
+          </div>
+        )}
       </div>
     </div>
   );
